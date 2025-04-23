c36fa541318a0fc2109e715f2b2d741e9516872e
giovannibragasv <giovannibragav@gmail.com>
2025-04-23 00:42:59 -0300

    feat: add recursive Fibonacci function implementation

---
+++ recursive.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int fib_calc(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    
+    return fib_calc(n-1) + fib_calc(n-2);
+}
+
+int fibonacci(int n) {
+    printf("Sequência de Fibonacci até o termo %d:\n", n);
+    
+    for (int i = 0; i <= n; i++) {
+        printf("%d ", fib_calc(i));
+    }
+    printf("\n");
+    
+    return fib_calc(n);
+}
+
+int main() {
+    int num;
+    
+    printf("Insira um número: ");
+    scanf("%d", &num);
+    
+    int fibo = fibonacci(num);
+    
+    printf("Fibonacci(%d) = %d\n", num, fibo);
+    
+    return 0;
+}
