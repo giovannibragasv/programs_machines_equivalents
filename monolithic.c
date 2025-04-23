84ded4aa0f196c84175b128c2d9c7483aaf3e14b
giovannibragasv <giovannibragav@gmail.com>
2025-04-23 00:42:30 -0300

    feat: enhance fibonacci function output with additional prints

--- monolithic.c
+++ monolithic.c
@@ -6,7 +6,10 @@ int fibonacci(int n) {
     int i = 1;
     int temp;
 
+    printf("%d ", a);    
     if (n == 0) goto end_0;
+    
+    printf("%d ", b);
     if (n == 1) goto end_1;
 
     loop:
@@ -14,14 +17,31 @@ int fibonacci(int n) {
         temp = a + b;
         a = b;
         b = temp;
+        printf("%d ", b);
         i++;
         goto loop;
 
     end_0:
+        printf("\n");
         return 0;
     end_1:
+        printf("\n");
         return 1;
     end_b:
+        printf("\n");
         return b;
+}
 
+int main() {
+    int num;
+
+    printf("Insira um número: ");
+    scanf("%d", &num);
+
+    printf("Sequência de Fibonacci até o termo %d:\n", num);
+    int fibo = fibonacci(num);
+    
+    printf("fibonacci(%d) = %d\n", num, fibo);
+    
+    return 0;
 }
